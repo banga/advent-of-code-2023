d10731e77715cdf84650b9dfa721f639870eddaf
Shrey Banga <banga.shrey@gmail.com>
2023-12-21 18:11:02 -0500

    Day 20 part 2
    Thought there might be something cyclical going on, but couldn't quite
    figure it out. After reading reddit, realized that the graph has
    clusters that can be broken down and measured separately. Lesson learned
    – examine the actual input!

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,9 +1,11 @@
 use std::{
     collections::{HashMap, VecDeque},
     fmt::Display,
+    iter,
 };
 
 use aoc2023::lib;
+use num::Integer;
 
 #[derive(Debug)]
 struct Pulse {
@@ -86,9 +88,7 @@ impl Module {
     }
 }
 
-pub fn part1() {
-    let lines = lib::read_lines();
-
+fn initialize_modules(lines: Vec<String>) -> (HashMap<String, Module>, String) {
     let mut in_edges = HashMap::<String, Vec<String>>::new();
     let mut out_edges = HashMap::<String, Vec<String>>::new();
     let mut modules = HashMap::<String, Module>::new();
@@ -137,6 +137,8 @@ pub fn part1() {
         modules.insert(name, module);
     }
 
+    // print_graph(&out_edges, &modules);
+
     for name in in_edges.keys() {
         modules.entry(name.clone()).or_insert(Module {
             name: name.clone(),
@@ -148,8 +150,33 @@ pub fn part1() {
     // Initialize the memories for conjunction modules
     for (name, module) in &mut modules {
         module.initiaze_in_edges(in_edges.get(name).unwrap());
-        println!("{:?}", module);
     }
+    (modules, broadcaster)
+}
+
+fn print_graph(edges: &HashMap<String, Vec<String>>, modules: &HashMap<String, Module>) {
+    println!("digraph main {{");
+    println!("rx [color=\"red\"]");
+    for (name, module) in modules {
+        match module.state {
+            ModuleState::FlipFlop { .. } => println!("{} [shape=\"square\"]", name),
+            ModuleState::Conjunction { .. } => {
+                println!("{} [shape=\"diamond\"] [color=\"green\"]", name)
+            }
+            _ => {}
+        }
+    }
+
+    for (name, neighbors) in edges {
+        for neighbor in neighbors {
+            println!("{} -> {}", name, neighbor);
+        }
+    }
+    println!("}}");
+}
+
+pub fn part1() {
+    let (mut modules, broadcaster) = initialize_modules(lib::read_lines());
 
     // Begin emulation
     let mut num_low = 0;
@@ -187,9 +214,80 @@ pub fn part1() {
     println!("{}x{} = {}", num_low, num_high, num_low * num_high);
 }
 
-pub fn part2() {}
+fn get_cycle_length(
+    modules: &mut HashMap<String, Module>,
+    broadcaster: &str,
+    destination: &str,
+) -> u64 {
+    let mut i = 0;
+    loop {
+        i += 1;
+
+        let mut bus = VecDeque::<Pulse>::new();
+
+        // Push button
+        bus.push_back(Pulse {
+            source: None,
+            target: broadcaster.to_string(),
+            is_high: false,
+        });
+
+        while let Some(pulse) = bus.pop_front() {
+            // println!("{}", pulse);
+
+            let Pulse {
+                source,
+                target,
+                is_high,
+            } = pulse;
+
+            if target == destination && !is_high {
+                return i;
+            }
+
+            let module = modules.get_mut(&target).unwrap();
+            module.apply_pulse(source, is_high, &mut bus);
+        }
+    }
+}
+
+pub fn part2() {
+    let lines = lib::read_lines();
+    let mut cycle_lengths = vec![];
+
+    for (start, end) in ["jl", "jr", "rp", "rt"]
+        .iter()
+        .zip(["jd", "vm", "fv", "lm"].iter())
+    {
+        let lines = lines
+            .iter()
+            .filter(|line| {
+                if line.starts_with("broadcaster") {
+                    return false;
+                }
+                if line.ends_with("-> zg") && !line.contains(&format!("{} ->", end)) {
+                    return false;
+                }
+                return true;
+            })
+            .chain(iter::once(&format!("broadcaster -> {}", start)))
+            .map(|s| s.clone())
+            .collect();
+
+        let (mut modules, broadcaster) = initialize_modules(lines);
+        let cycle_length = get_cycle_length(&mut modules, &broadcaster, "rx");
+        cycle_lengths.push(cycle_length);
+    }
+
+    println!("{:?}", cycle_lengths);
+    let mut min_length = cycle_lengths[0];
+    for l in cycle_lengths {
+        min_length = min_length.lcm(&l);
+    }
+    println!("{}", min_length);
+}
 
 pub fn main() {
-    part1();
-    // part2();
+    // part1();
+    part2();
 }
