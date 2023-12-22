17b2ca186be1e890dad2a527d1210a0f52cb3231
Shrey Banga <banga.shrey@gmail.com>
2023-12-22 13:38:16 -0500

    Day 20 part 2
    Did not solve this myself. I did wonder if there would be more cycle
    finding here, but this one required either examining the input very
    carefully or more leaps of the imagination.

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -10,15 +10,16 @@ fn dfs(
     budget: isize,
     visited: &mut HashSet<(isize, isize, isize)>,
 ) {
-    if i < 0 || i >= grid.len() as isize || j < 0 || j >= grid.len() as isize {
-        return;
-    }
     let key = (i, j, budget);
-    if !visited.insert(key) {
+    let h = grid.len() as isize;
+    let w = grid[0].len() as isize;
+    let gi = i.rem_euclid(h) as usize;
+    let gj = j.rem_euclid(w) as usize;
+    let ch = grid[gi as usize][gj as usize];
+    if ch == b'#' {
         return;
     }
-    let ch = grid[i as usize][j as usize];
-    if ch == b'#' {
+    if !visited.insert(key) {
         return;
     }
     if budget == 0 {
@@ -28,38 +29,6 @@ fn dfs(
     dfs(grid, i + 1, j, budget - 1, visited);
     dfs(grid, i, j - 1, budget - 1, visited);
     dfs(grid, i, j + 1, budget - 1, visited);
-}
-
-// Check if (si, sj) can be reached from (i, j) with a path of length exactly `length`
-fn dp(
-    grid: &Vec<Vec<u8>>,
-    si: isize,
-    sj: isize,
-    i: isize,
-    j: isize,
-    length: isize,
-    cache: &mut HashMap<(isize, isize, isize), bool>,
-) -> bool {
-    // assert!(length >= 0);
-    if i < 0 || i >= grid.len() as isize || j < 0 || j >= grid[0].len() as isize {
-        return false;
-    }
-    if grid[i as usize][j as usize] == b'#' {
-        return false;
-    }
-    if length == 0 {
-        return i == si && j == sj;
-    }
-    let key = (i, j, length);
-    if let Some(value) = cache.get(&key) {
-        return *value;
-    }
-    let neighbors = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)];
-    let value = neighbors
-        .iter()
-        .any(|(ni, nj)| dp(grid, si, sj, *ni, *nj, length - 1, cache));
-    cache.insert(key, value);
-    value
 }
 
 #[allow(dead_code)]
@@ -96,9 +65,43 @@ fn part1() {
 }
 
 #[allow(dead_code)]
-fn part2() {}
+fn part2() {
+    let grid = lib::read_byte_lines();
+
+    let mut si = 0;
+    let mut sj = 0;
+    'outer: while si < grid.len() {
+        while sj < grid[0].len() {
+            if grid[si][sj] == b'S' {
+                break 'outer;
+            }
+            sj += 1;
+        }
+        sj = 0;
+        si += 1;
+    }
+
+    // This is all based on hints from reading other people's solutions:
+    // - The number 26501365 is a 202300 * 131 + 65
+    // - The output is periodic in the width of the grid, which is 131
+    // - If you plot the values for any starting offset, say 65, you will see
+    // that its second derivative is a constant = 31098.
+
+    // So, the output is described by a quadratic function f on the budget size,
+    // which can be calculated using the first three values. Then the final
+    // answer is f(202300).
+    for budget in (65..600).step_by(131) {
+        let mut visited = HashSet::new();
+        dfs(&grid, si as isize, sj as isize, budget, &mut visited);
+        println!(
+            "{} {}",
+            budget,
+            visited.iter().filter(|(_, _, dist)| *dist == 0).count(),
+        );
+    }
+}
 
 pub fn main() {
-    part1();
-    // part2();
+    // part1();
+    part2();
 }
