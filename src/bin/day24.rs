5970db278653a5a1c9eb1d7d9f6473b65745eb62
Shrey Banga <banga.shrey@gmail.com>
2023-12-26 15:48:28 -0500

    Day 24 part 2
    I got to this by reading how other people did it. A common theme was assuming that the velocities would be small and brute forcing it.
    
    If we know the start velocity, we have three unknowns (x, y, z) and 1 unknown per line (t_i). So if we consider two lines, we get 5 unknowns and 6 equations. I solved for the 5 unknowns by representing them as a matrix multipliciation and inverting the matrix. For a given velocity, we compare the solutions for two different lines. The error between those should be very small for the right solution, so we print it out every time the error goes down.

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -5,6 +5,9 @@ use std::{
 };
 
 use aoc2023::lib::{self, print_lines};
+use nalgebra::{
+    matrix, Matrix4, Matrix5, Matrix5x1, Matrix5x6, Matrix6, Matrix6x1, Matrix6x5, SquareMatrix,
+};
 
 #[derive(Debug, PartialEq)]
 struct Vector {
@@ -15,8 +18,8 @@ struct Vector {
 
 impl Display for Vector {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // f.write_fmt(format_args!("({},{},{})", self.x, self.y, self.z))
-        f.write_fmt(format_args!("({},{})", self.x, self.y))
+        f.write_fmt(format_args!("({},{},{})", self.x, self.y, self.z))
+        // f.write_fmt(format_args!("({},{})", self.x, self.y))
     }
 }
 
@@ -63,9 +66,8 @@ impl Line {
     // fn intersect(&self, other: &Self) -> Option<Vector> {}
 }
 
-#[allow(dead_code)]
-fn part1(lines: Vec<String>, bounds: (f64, f64)) {
-    let lines: Vec<_> = lines
+fn parse_lines(input: Vec<String>) -> Vec<Line> {
+    input
         .iter()
         .map(|line| {
             let (pos, vel) = line.split_once(" @ ").unwrap();
@@ -90,9 +92,13 @@ fn part1(lines: Vec<String>, bounds: (f64, f64)) {
                 },
             }
         })
-        .collect();
+        .collect()
+}
 
+#[allow(dead_code)]
+fn part1(input: Vec<String>, bounds: (f64, f64)) {
     let mut count = 0;
+    let lines = parse_lines(input);
     for i in 0..lines.len() {
         let line1 = &lines[i];
         for j in 0..i {
@@ -111,9 +117,68 @@ fn part1(lines: Vec<String>, bounds: (f64, f64)) {
     println!("{}", count);
 }
 
+fn solve(dx: isize, dy: isize, dz: isize, l1: &Line, l2: &Line) -> Option<Matrix5x1<f64>> {
+    let m: Matrix5<f64> = matrix![
+        1.0, 0.0, 0.0, dx as f64 - l1.velocity.x, 0.0;
+        0.0, 1.0, 0.0, dy as f64 - l1.velocity.y, 0.0;
+        0.0, 0.0, 1.0, dz as f64 - l1.velocity.z, 0.0;
+        1.0, 0.0, 0.0, 0.0, dx as f64 - l2.velocity.x;
+        0.0, 1.0, 0.0, 0.0, dy as f64 - l2.velocity.y;
+    ];
+    if let Some(inv) = m.try_inverse() {
+        let n = matrix![
+            l1.position.x;
+            l1.position.y;
+            l1.position.z;
+            l2.position.x;
+            l2.position.y;
+        ];
+        // Contains [x; y; z; t1; t2]
+        let soln = inv * n;
+        if soln[3] <= 0.0 || soln[4] <= 0.0 {
+            return None;
+        }
+        return Some(soln);
+    }
+    None
+}
+
 #[allow(dead_code)]
-fn part2() {
-    // println!("{}", num_fallen);
+fn part2(input: Vec<String>) {
+    let lines = parse_lines(input);
+    let mut min_err = 1000_000.0;
+    let mut min_pos = (0.0, 0.0, 0.0);
+    for dx in 0..1000 {
+        for dy in 0..1000 {
+            for dz in 0..1000 {
+                if let Some(sol1) = solve(dx, dy, dz, &lines[0], &lines[1]) {
+                    if let Some(sol2) = solve(dx, dy, dz, &lines[1], &lines[2]) {
+                        let err = [sol1[0] - sol2[0], sol1[1] - sol2[1], sol1[2] - sol2[2]]
+                            .iter()
+                            .map(|v| v.abs())
+                            .sum();
+                        if err < min_err {
+                            min_err = err;
+                            min_pos = (sol1[0], sol1[1], sol1[2]);
+                            println!("{:?} err={}", min_pos, min_err);
+                            println!("{} {} {}", dx, dy, dz);
+                            println!(
+                                "{} {} {}",
+                                sol1[0] + dx as f64 * sol1[3],
+                                sol1[1] + dy as f64 * sol1[3],
+                                sol1[2] + dz as f64 * sol1[3],
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("min_pos={:?} err={}", min_pos, min_err);
+    println!(
+        "{}",
+        min_pos.0.round() + min_pos.1.round() + min_pos.2.round()
+    );
 }
 
 #[test]
@@ -129,11 +194,12 @@ fn test() {
     .split('\n')
     .map(|s| s.to_string())
     .collect();
-    part1(input, (7.0, 27.0));
+    // part1(input, (7.0, 27.0));
+    part2(input);
 }
 
 pub fn main() {
     let input = lib::read_lines();
-    part1(input, (200000000000000.0, 400000000000000.0));
-    // part2(input);
+    // part1(input, (200000000000000.0, 400000000000000.0));
+    part2(input);
 }
